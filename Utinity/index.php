95e3923a51e1e3e21b9a6891cdda7186b1cc9235
taro.yamada <taro.yamada@example.com>
2016-01-21 21:29:31 +0900

    commit

--- Utinity/index.php
+++ Utinity/index.php
@@ -12,12 +12,11 @@
 		<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 		<![endif]-->
 	<style>
-
-a.thumbnail {
-    vertical-align: middle;
-    display: table-cell;
-    background:white;
-}
+		a.thumbnail {
+			vertical-align: middle;
+			display: table-cell;
+			background:white;
+		}
 	</style>
 	<script src="//code.jquery.com/jquery.js"></script>
 	<script>
@@ -90,113 +89,115 @@ a.thumbnail {
 							<p><b>Note:</b> </p>
 						</div>
 					</div>
-
-<?php 
-
-$array = [
-	"The CodePlayer"=> [
-		"The CodePlayer",
-		"http://thecodeplayer.com/",
-		"thecodeplayer.png",
-	],
-	"Codecademy"=> [
-		"Codecademy",
-		"http://www.codecademy.com/#!/exercises/0",
-		"codecademy.png",
-	],
-	"Udacity"=> [
-		"Udacity",
-		"https://www.udacity.com/how-it-works",
-		"udacity.png",
-	],
-	"Code School"=> [
-		"Code School",
-		"https://www.codeschool.com/",
-		"Code School.png",
-	],
-	"Dash"=> [
-		"Dash",
-		"https://dash.generalassemb.ly/",
-		"dash.png",
-	],
-	"W3schools"=> [
-		"W3schools",
-		"http://www.w3schools.com/",
-		"w3schools.png",
-	],
-	"MIT OpenCourseWare"=> [
-		"MIT OpenCourseWare",
-		"http://ocw.mit.edu/courses/electrical-engineering-and-computer-science/",
-		"mit.edu.png",
-	],
-	"Mozilla Developer Network"=> [
-		"Mozilla Developer Network",
-		"https://developer.mozilla.org/en-US/",
-		"Mozilla Developer Network.jpg",
-	],
-
-	"Coursera"=> [
-		"Coursera",
-		"https://www.coursera.org/",
-		"coursera.png",
-	],
-	"Khan Academy"=> [
-		"Khan Academy",
-		"https://www.khanacademy.org/cs/tutorials/programming-basics",
-		"khan.png",
-	],
-	"Learn Python the Hard Way"=> [
-		"Learn Python the Hard Way",
-		"http://learnpythonthehardway.org/",
-		"learnpython.png",
-	],
-	"HTML5 Rocks"=> [
-		"HTML5 Rocks",
-		"http://www.html5rocks.com/en/",
-		"html5rock.png",
-	]
-];
-$order = [
-"The CodePlayer",
-"Codecademy",
-"W3schools",
-"Code School",
-"HTML5 Rocks",
-"Mozilla Developer Network",
-"Learn Python the Hard Way",
-"Udacity",
-"Dash",
-"MIT OpenCourseWare",
-"Coursera",
-"Khan Academy",
-];
-
- ?>
-
-
-
- 
+					<?php
+							$array_website = [
+								"The CodePlayer"=> [
+									"name_web"=>"The CodePlayer",
+									"link_web"=>"http://thecodeplayer.com/",
+									"img_preview"=>"thecodeplayer.png",
+									"note"=>"Đây là một trang web đáng xem",
+								],
+								"Codecademy"=> [
+									"name_web"=>"Codecademy",
+									"link_web"=>"http://www.codecademy.com/#!/exercises/0",
+									"img_preview"=>"codecademy.png",
+									"note"=>"",
+								],
+								"Udacity"=> [
+									"name_web"=>"Udacity",
+									"link_web"=>"https://www.udacity.com/how-it-works",
+									"img_preview"=>"udacity.png",
+									"note"=>"",
+								],
+								"Code School"=> [
+									"name_web"=>"Code School",
+									"link_web"=>"https://www.codeschool.com/",
+									"img_preview"=>"Code School.png",
+									"note"=>"",
+								],
+								"Dash"=> [
+									"name_web"=>"Dash",
+									"link_web"=>"https://dash.generalassemb.ly/",
+									"img_preview"=>"dash.png",
+									"note"=>"",
+								],
+								"W3schools"=> [
+									"name_web"=>"W3schools",
+									"link_web"=>"http://www.w3schools.com/",
+									"img_preview"=>"w3schools.png",
+									"note"=>"",
+								],
+								"MIT OpenCourseWare"=> [
+									"name_web"=>"MIT OpenCourseWare",
+									"link_web"=>"http://ocw.mit.edu/courses/electrical-engineering-and-computer-science/",
+									"img_preview"=>"mit.edu.png",
+									"note"=>"",
+								],
+								"Mozilla Developer Network"=> [
+									"name_web"=>"Mozilla Developer Network",
+									"link_web"=>"https://developer.mozilla.org/en-US/",
+									"img_preview"=>"Mozilla Developer Network.jpg",
+									"note"=>"",
+								],
+								"Coursera"=> [
+									"name_web"=>"Coursera",
+									"link_web"=>"https://www.coursera.org/",
+									"img_preview"=>"coursera.png",
+									"note"=>"",
+								],
+								"Khan Academy"=> [
+									"name_web"=>"Khan Academy",
+									"link_web"=>"https://www.khanacademy.org/cs/tutorials/programming-basics",
+									"img_preview"=>"khan.png",
+									"note"=>"",
+								],
+								"Learn Python the Hard Way"=> [
+									"name_web"=>"Learn Python the Hard Way",
+									"link_web"=>"http://learnpythonthehardway.org/",
+									"img_preview"=>"learnpython.png",
+									"note"=>"",
+								],
+								"HTML5 Rocks"=> [
+									"name_web"=>"HTML5 Rocks",
+									"link_web"=>"http://www.html5rocks.com/en/",
+									"img_preview"=>"html5rock.png",
+									"note"=>"",
+								]
+							];
+							$order = [
+							"The CodePlayer",
+							"Codecademy",
+							"W3schools",
+							"Code School",
+							"HTML5 Rocks",
+							"Mozilla Developer Network",
+							"Learn Python the Hard Way",
+							"Udacity",
+							"Dash",
+							"MIT OpenCourseWare",
+							"Coursera",
+							"Khan Academy",
+							];
+						?>
+					<hr>
 					<h2>Education</h2>
+					<h4>Một số trang web dạy lập trình</h4>
 					<div class="row">
-<?php
-foreach ($order as $key => $value) {
-	$value=$array[$value]
-	?>
-		<div class="col-md-3 col-ele">
-			<a href="<?= $value[1]; ?>" class="thumbnail" target="_blank">
-				<img src="img/<?= $value[2]; ?>">
-			</a>
-			<div class="text-center"><h3><?= $value[0]; ?></h3></div>
-			<p><b>Note:</b> </p>
-		</div>
-	<?php
-}
- ?>
+						<?php
+						foreach ($order as $key => $value) {
+							$value=$array_website[$value];
+							?>
+								<div class="col-md-3 col-ele text-cente"r>
+									<a href="<?= $value["link_web"]; ?>" class="thumbnail" target="_blank">
+										<img src="img/<?= $value["img_preview"]; ?>">
+									</a>
+									<div class="text-center"><h3><?= $value["name_web"]; ?></h3></div>
+									<p><b>Note:</b> <?= $value["note"]; ?> </p>
+								</div>
+							<?php
+						}
+						?>
 					</div>
-
-
-
-				
 				<!-- END panel-body -->
 			</div>
 			<!-- END panel -->

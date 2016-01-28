1a130c776024aa47d186c7cda372a7e9f0bb5489
taro.yamada <taro.yamada@example.com>
2016-01-28 11:14:45 +0900

    cropper.js

--- Utinity/Cropper/imagick.php
+++ Utinity/Cropper/imagick.php
@@ -1,11 +1,36 @@
 <?php
+	//============ ============  ============ ============
+	//  Param input necessary
+	// $_POST["width"]
+	// $_POST["height"]
+	// $_POST["x"]
+	// $_POST["y"]
+	// $_POST["path_file"]
+	//
+	//============ ============  ============ ============
+	$_POST["path_file"] = "img/20160127193911.original.jpeg";
+	if(
+		!(
+			$_POST["width"] &&
+			$_POST["height"] &&
+			$_POST["x"] &&
+			$_POST["y"] &&
+			$_POST["path_file"]
+		)
+	){
+		die;
+	}
+	//$_POST["path_file"] = "img/20160127193911.original.jpeg";
 
-  print_r($_POST);
-  extract($_POST);
-  $image = new Imagick("img/20160127193911.original.jpeg");
-  $image->cropImage(  $width ,  $height ,  $x ,  $y );
-  $image->writeImage("xxx".time().".png");
-  echo $cropped;
+	// ============ ============  ============ ============
+	// Rã biến Post ra thành các biến con
+	// [$_POST["x"] => $x]
+	extract($_POST);
+	// Khởi tạo đối tượng Imagick với nội dung là file input
+	$image = new Imagick($path_file);
+	// Crop hình theo thông số truyền vào
+	$image->cropImage(  $width ,  $height ,  $x ,  $y );
+	// Lưu file xuống
+	$image->writeImage("xxx".time().".png");
 
-
- ?>
+?>

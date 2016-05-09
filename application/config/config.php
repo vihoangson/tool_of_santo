8d225e7b58f4d884fb7bba9eaa4f386870e7b6b8
vihoangson <vihoangson@gmail.com>
2016-05-09 15:03:52 +0700

    commit

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://tool.vihoangson.com.dev.vn/';
+$config['base_url'] = 'http://tool.vihoangson.com/';
 
 /*
 |--------------------------------------------------------------------------
